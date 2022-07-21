a68377b63cefe0a77cbed1e3b876c1fc4664860f
sam-7073 <109744243+sam-7073@users.noreply.github.com>
2022-07-21 21:58:40 +0600

    002

---
+++ hello1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+ 
+ , 
+using namespace std;
+int main(){
+    #ifndef ONLINE_JUDGE
+       freopen("input1.txt","r", stdin);
+       freopen("output1.txt","w", stdout);
+    #endif
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<"\n";
+
+
+
+    return 0;
+}
